fad9f204cac85c1fa0b1edc439b455b67c5e610a
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-01-24 10:36:24 +0500

    Update Learn.py

--- Learn.py
+++ Learn.py
@@ -1,10 +1,12 @@
-print("This is python") /*String 
-print(234)/*Integer
-print(3.789776)/*Float
-print("1655327") /*String 
+print("This is python") #String 
+print(234)#Integer
+print(3.789776)#Float
+print("1655327")#String 
 #comment
 name = input("what's your name")
 print("hello", name)
 
-#variable is name
+#variable is name 
 
+age = 22
+print (age)
