93cd333524d674d86bb21fedb576e4b0c9d8a3c8
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-01-28 22:03:51 +0500

    Update Python_for_DataAnalytics.py

---
+++ Python_for_DataAnalytics.py
@@ -0,0 +1,14 @@
+print("I am a Data Nerd")
+#  Commenting we use "#"
+
+"""
+This is for multi line comments use delimiters
+
+
+"""
+#Numeric Functions
+print(1*4)
+print(4*6)
+print(4*6+6)#Bodmas Rule  (Bracket of Division,Multiplication,addition,Substraction)
+
+
