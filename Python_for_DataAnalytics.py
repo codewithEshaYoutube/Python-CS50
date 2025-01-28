e7a0307204411e152183a285de1d8618f9917256
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-01-28 22:12:12 +0500

    Update Python_for_DataAnalytics.py

--- Python_for_DataAnalytics.py
+++ Python_for_DataAnalytics.py
@@ -20,3 +20,10 @@ base_salary = 10000
 bonus_rate = 1.7
 Total_salary = (base_salary)*(1+ bonus_rate)
 print(Total_salary)
+"""
+Runtime result:
+The slowest run took 16.13 times longer than the fastest. This could mean that an intermediate result is being cached.
+327 µs ± 216 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
+
+
+"""
