6dd26dc8fdb7388dc2a1ef5bfcd67a7b79647eb2
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-05 00:21:36 +0500

    Update Scratch.py

--- Scratch.py
+++ Scratch.py
@@ -1,5 +1,5 @@
 course = ("This is python code")
-
+price=44
 #methods
 print(len(course))
 print(len(course))
@@ -10,4 +10,7 @@ print(course.find("is"))
 print(course.replace("is","is best"))
 #operator
 print('is' in course)
-#
+#old string method
+print(course, "price is ",[price],' USD')
+#formatted strings
+print(f'{course}  price is [{price}] USD')
