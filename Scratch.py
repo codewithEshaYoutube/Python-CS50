1cdc1ae2bddbaadabc8905f6cc9da5fe160b3f67
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-05 00:16:25 +0500

    Update Scratch.py

--- Scratch.py
+++ Scratch.py
@@ -3,5 +3,11 @@ course = ("This is python code")
 #methods
 print(len(course))
 print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find("is"))
 
-
+print(course.replace("is","is best"))
+#operator
+print('is' in course)
+#
