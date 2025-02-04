79b0bd92da3a860c512ebc045815646ee32661cc
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-04 23:15:27 +0500

    Update Scratch.py

--- Scratch.py
+++ Scratch.py
@@ -1,3 +1,7 @@
 birth_year=(input("What is your birth year:"))
 age =(2025 - int(birth_year))
 print(age)
+# km --> m
+distance = (input("How much distance you covered in km?  "))
+distance_covered= (1000 * int(distance))
+print (distance_covered)
