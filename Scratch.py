77409eaf5ad5a7cc5fe3abbff10966121fdf4da5
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-04 23:20:41 +0500

    Update Scratch.py

--- Scratch.py
+++ Scratch.py
@@ -6,3 +6,6 @@ distance = (input("How much distance you covered in km?  "))
 distance_covered= (1000 * int(distance))
 print (distance_covered)
 #kg --> lbs
+weight_kg = (input("what is your weight in kg?  "))
+weight_lb = (2.2* int(weight_kg))
+print(weight_lb)
