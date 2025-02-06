13b182b35bb705ddd2b75cbac382db00f32e5d43
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-06 19:23:21 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -7,3 +7,23 @@ class Solution:
     haystack = "sadbutsad"
     needle = "sad"
     88. Merge Sorted Array
+
+    class Solution:
+        def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+            """
+            Do not return anything, modify nums1 in-place instead.
+
+            """
+            x, y = m - 1, n - 1
+            for z in range(m + n - 1, -1, -1):
+                if y < 0:
+                    break
+                if x >= 0 and nums1[x] > nums2[y]:
+                    nums1[z] = nums1[x]
+                    x -= 1  # x=x-1
+                else:
+                    nums1[z] = nums2[y]
+                    y -= 1
+
+            # T = O(m+n)
+            # S = O(1)
