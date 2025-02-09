32e0bf22a870ce382e6755f9747fb6e35a5606c8
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-09 15:27:18 +0500

    update

--- Notepad.py
+++ Notepad.py
@@ -2,6 +2,4 @@ def count_unique_letters(s: str) -> int:
     # Convert the string to lowercase and remove non-alphabetic characters
     s = ''.join(filter(str.isalpha, s.lower()))
 
-    # Use a set to find unique letters
-    unique_letters = set(s)
-  
+    
