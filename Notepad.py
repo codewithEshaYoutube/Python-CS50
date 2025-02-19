8ff14c0eebe71656458da803bf74b3f04e8d99e4
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-20 00:12:47 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -8,14 +8,14 @@ understanding level is beginner
 course name is python for data analytics 
 '''
 #variables-> function->studentdetails-> call
-instiute_name = "corvit"
+institute_name = "corvit"
 student_name = "Ali"
 student_age= 12
 level = "beginner"
 student_is_new = True
 course_name= "python for data analytics"
 def print_student_details():
-    print(f"Institute Name:{instiute_name}")
+    print(f"Institute Name:{institute_name}")
     print(f"Student Name:{student_name}")
     print(f"Student_age:{student_age}")
     print(f"Understanding level:{level}")
