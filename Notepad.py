97c7699c8e7f3dfada23dce4f57ebfd6ea952112
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-19 18:45:37 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -4,3 +4,13 @@ def print_user_name():
     print(f"Hello {user_name}😊")
 print(print_user_name())
 #VARIABLE EXCERCISE
+'''
+institute is corvit
+student is Ali is new student 
+his age is 19 years
+understanding level is beginner
+course name is python for data analytics 
+
+
+
+'''
