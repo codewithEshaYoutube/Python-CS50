e91e583a620bcbea09d1293d5d28bb92e1a6c261
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-21 19:42:15 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -1,15 +1,6 @@
 
 
-class  Solution:
-    def roman_to_int(nums):
-        roman_map = [("M",1000),("CM",900),("D",500),("CD",400),
-                     ("C",100),("XC",90),("L",50),("X",10),("V",5),("IV",4),("I",1)]
-        numeral = " "
-        for value,symbol in roman_map:
-            while nums >=value:
-                numeral += symbol
-                nums-=value
-            return numeral
+
 
 
 
