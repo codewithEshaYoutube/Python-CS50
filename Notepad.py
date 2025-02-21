6636e1cf99caab17c216c287246e4fc39a7b4fb6
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-21 19:30:59 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -1,27 +1,15 @@
 
-#VARIABLE EXCERCISE
-'''
-institute is corvit
-Student is Ali is new student
-his age is 19 years
-understanding level is beginner
-course name is python for data analytics 
-'''
-#variables-> function->studentdetails-> call
-institute_name = "corvit"
-student_name = "Ali"
-student_age= 12
-level = "beginner"
-student_is_new = True
-course_name= "python for data analytics"
-def print_student_details():
-    print(f"Institute Name:{institute_name}")
-    print(f"Student Name:{student_name}")
-    print(f"Student_age:{student_age}")
-    print(f"Understanding level:{level}")
-    print(f"Course Name:{course_name}")
-print(print_student_details())
-
 
 class  Solution:
     def roman_to_int(nums):
+        roman_map = [("M",1000),("CM",900),("D",500),("CD",400),
+                     ("C",100),("XC",90),("L",50),("X",10),("V",5),("IV",4),("I",1)]
+        numeral = " "
+        for value,symbol in roman_map:
+            while nums >=value:
+                numeral += symbol
+                nums-=value
+            return numeral
+
+
+
