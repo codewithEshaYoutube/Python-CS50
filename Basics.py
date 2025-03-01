dd1c2f9d7847b52c94346fd1c1c5f1eb8be8901a
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-01 19:36:58 +0500

    update

--- Basics.py
+++ Basics.py
@@ -289,3 +289,24 @@ customer={
 customer["age"]=20
 customer["Smart_student"]= True
 print(customer)
+'''
+Phone number is requested and give them nums in words
+'''
+nums=input("Enter your phone number  ")
+
+Numbers_in_mapping = {
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    "0": "zero"
+}
+output=""
+
+output="".join(Numbers_in_mapping.get(ch,"!")for ch in nums)
+print (output)
--- Notepad.py
+++ Notepad.py
@@ -1,21 +1,8 @@
-'''
-Phone number is requested and give them nums in words
-'''
-nums=input("Enter your phone number  ")
-
-Numbers_in_mapping = {
-    "1": "one",
-    "2": "two",
-    "3": "three",
-    "4": "four",
-    "5": "five",
-    "6": "six",
-    "7": "seven",
-    "8": "eight",
-    "9": "nine",
-    "0": "zero"
+"""
+emoji converter
+"""
+emoji_mapping={
+        ":)" :"😊",
+        ":(" :"😢",
+        ";)" : "😉"
 }
-output=""
-
-output="".join(Numbers_in_mapping.get(ch,"!")for ch in nums)
-print (output)
