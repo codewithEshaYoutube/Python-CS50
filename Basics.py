b87666ba6a73ad19f576c55da3b3c538278c984d
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-01 12:21:16 +0500

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -62,3 +62,17 @@ while e <=10:
 print('done')
 
 #guess game using while loop
+secret_num=7
+guess_count=0
+guess_limit=4
+while guess_count<guess_limit:
+    guess=int(input("Guess the num:  "))
+    guess_count+=1
+    if guess == secret_num:
+        print("You won")
+        break
+    else:
+        print("You failed,Try again")
+
+
+
