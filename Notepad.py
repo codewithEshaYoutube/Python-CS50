cc06177a6b973fcf75d8ec32a1537f64cf021699
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-01 16:33:23 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -1,4 +1,9 @@
 
 #Challange to find largest num in list
-nums=[1,5,6,7,8,11,14]
-max(nums)
+nums=[1,5,6,7,8,11,14,78,3,1,33,27]
+max=nums[0]
+
+for num in nums:
+        if num>max:
+                max=num
+print (max)
