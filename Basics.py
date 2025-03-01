d59e19e026e37cd924709af8bb179cc177466c1c
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-01 15:47:08 +0500

    update

--- Basics.py
+++ Basics.py
@@ -148,3 +148,23 @@ for i in nums2:
 #lists
 names=["esh","sam","bob","mosh"]
 print(names)
+#if_statements
+"""
+weather is hot outside
+drinln plenty water
+and enjoy andgthes
+otherwisw its lovely weather
+
+"""
+is_hot=True
+is_cold=False
+
+if is_hot:
+        print("its hot outside")
+        print("drink plenty water")
+elif is_cold:
+        print("its cold outside")
+        print("wear warm clothes")
+else:
+        print("its lovely weather")
+print("enjoy your day")
