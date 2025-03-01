7a723dbd245a3294ab68fce43bc00b94bd042e7b
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-01 16:08:51 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -3,6 +3,12 @@ logical operators
 Example 1: if employee has high income and credit than eligible for loan
 """
 high_income=True
-high_credit=True
+high_credit=False
+is_adult=True
 if high_income and high_credit :
         print("Employee is eligible")
+elif not is_adult:
+        print("Candidate is underage")
+        
+else:
+        print('not eligible')
