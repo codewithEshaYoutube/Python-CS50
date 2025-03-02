79a6bb99e3565706720b1f5648218ebe5ab0f2e3
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-02 15:34:42 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -1,8 +1,9 @@
 """
 Defining functions
 """
+
 def student_detail():
-        name=input("whatis you name? ")
+        name = input("whatis you name? ")
         print(f"hi there {name}")#parameter
         print("age is 19")
 print (student_detail())
