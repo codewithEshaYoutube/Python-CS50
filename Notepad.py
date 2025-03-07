59ea01ff4115bfd957eb245f29511d925dc43d80
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-07 17:06:15 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -1,8 +1,8 @@
-def greet(name ):
-    print(f"HellO {name}")
+def greet(name,last_name ):
+    print(f"HellO {name}{last_name}")
     print("Welcome to class")
 print("Start")
-greet("Esha")
+greet("Esha","Tariq")
 print ("Finish")
 #Argument are pieces of information that is given to function
-#Parameter defined in function
+#Parameter defined in function 
