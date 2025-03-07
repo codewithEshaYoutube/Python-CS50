49763a43573f578c90855095b786208ae85b53dd
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-07 17:15:44 +0500

    update

--- Basics.py
+++ Basics.py
@@ -337,7 +337,20 @@ def student_detail():
         print("age is 19")
 print (student_detail())
 print (student_detail())
- """
- parameters
- """
- def greet(name,class):
+
+
+def greet(name, last_name):
+    print(f"HellO {name}{last_name}")
+    print("Welcome to class")
+
+
+print("Start")
+greet("Esha", "Tariq")
+greet("Esha", "Tariq")
+print("Finish")
+# Parameter defined in function
+"""
+Argument are pieces of information that is given to function
+Positonal arguments:no keyword first define this 
+Keyword argument: defined with keyword
+"""
