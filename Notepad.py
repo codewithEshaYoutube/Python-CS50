7774c636ef70174f3074cdd35f5cbb4acbe41541
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-07 17:15:06 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -3,9 +3,11 @@ def greet(name,last_name ):
     print("Welcome to class")
 print("Start")
 greet("Esha","Tariq")
+greet("Esha","Tariq")
 print ("Finish")
 #Parameter defined in function
 """
 Argument are pieces of information that is given to function
 Positonal arguments:no keyword first define this 
+Keyword argument: defined with keyword
 """
