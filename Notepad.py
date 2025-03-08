2a5c3364949ab828941479546a90813779ccae16
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-08 12:50:35 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -3,4 +3,4 @@ try:
     user=int(input("what is your age "))
     print(user)
 except ValueError:
-    print("Invalid Value,Value s   hould be a Number")
+    print("Invalid Value,Value s hould be a Number")
