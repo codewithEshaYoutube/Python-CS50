646fcd2145f987e6e60e8797046fc10e97b834de
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-19 21:36:20 +0500

    Update practise.py

--- practise.py
+++ practise.py
@@ -1,3 +1,7 @@
 #
 mars_multiple=0.378
-de main()
+def main():
+    earth_weight=input("What is your weight on earth ")
+    mars_weight=(mars_multiple*earth_weight)
+    print(f"your weight on mars is{mars_weight} ")
+
