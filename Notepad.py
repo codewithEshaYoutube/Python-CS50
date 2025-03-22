1896b7a3ac926baa31d1cc9469b29bcf58dc6854
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-22 21:55:58 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -1,5 +1,7 @@
-args=[]
-Str = "Hello World"
-def hello_world():
-    if args!= str:
-        print("Hello World")
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        yes=input(" ")
+        hash_map={"a":"dog", "b":"cat"
+        }
+        print(a:b)
+    wordPattern()
