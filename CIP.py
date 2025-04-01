6b731304ae06f386774042a09f61274ed08faba8
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-04-01 13:10:02 +0500

    Update CIP.py

--- CIP.py
+++ CIP.py
@@ -1,3 +1,7 @@
 mars_multiple=0.397
 def mass_converter():
     weight_on_Earth=input("What is your weight at Mars? ")
+    weight_on_Earth_int=int(weight_on_Earth)
+    weigth_on_Mars=weight_on_Earth_int  * mars_multiple
+    print(f"This is  your weight on  Mars {weigth_on_Mars}")
+    
