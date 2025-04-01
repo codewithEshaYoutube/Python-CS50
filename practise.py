e5d2c9b276d87eeb61482b8bb1643dc3f98ce14f
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-04-01 13:24:50 +0500

    Update practise.py

---
+++ practise.py
@@ -0,0 +1,16 @@
+#
+mars_multiple=0.378
+def main():
+    earth_weight=input("What is your weight on earth?  ")
+    earth_weight_int=int(earth_weight)
+    mars_weight=(mars_multiple*earth_weight_int)
+    print(f"your weight on mars is : {mars_weight} ")
+
+if __name__=="__main__":
+    main()
+ #defining function
+ #input taking from  user
+ #typecasting
+ #concatenation
+ #formula defining
+ #recalling
