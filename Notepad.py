7519cd649bedb1131d9d669fde1f72de09412ebe
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-04-08 00:51:12 +0500

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -1,3 +1,22 @@
-from Problems_test import MARS_MULTIPLE
+# defining constant as mars multiple
+MARS_MULTIPLE = 0.378
 
-MARS_MULTIPLE
+
+def main():
+    # declaring Variable and getting input from user
+    earth_mass = input("what is your weight on earth? ")
+
+    # the input in string to float is called typecasting
+    earth_mass_float = float(earth_mass)
+
+    # Defining Mars weight calculation formula and assigning variable
+    mars_mass = (MARS_MULTIPLE * earth_mass_float)
+
+    # printing string concatenation (combining two strings into one)
+    print("this is equivalent weight on Mars" + str(mars_mass))
+
+
+if __name__ == "__main__":
+    main()
+
+
