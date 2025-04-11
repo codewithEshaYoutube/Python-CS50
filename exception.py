2bdcd77df8d8eccb29c8a17c4bcda8cc8e63f3e5
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-04-11 19:56:50 +0500

    Update exception.py

--- exception.py
+++ exception.py
@@ -1,6 +1,5 @@
 #this repo is all about how we gonna handle exceptions
-try:
-    age=int(input("age:"))
-    print(age)
-except ValueError:
-    print("invalid value")
+
+age=int(input("age:"))
+print(age)
+
