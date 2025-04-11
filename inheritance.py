8d7b18f81442ffd9749aedd7e31de1d3d85f4dad
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-04-11 21:50:05 +0500

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,1 +1,19 @@
 #whatevr w put in parent it child will get own methods and parent methods too
+class Mammal:
+    def hairs(self,name,age):
+        self.name= name
+        self.age= age
+        print("have hairs")
+class Dog(Mammal):
+    def annoying(self):
+        print("bark at you")
+class Cat(Mammal):
+    def meow(self):
+        print("meow meow meow")
+    def cute(self):
+        print("cats are cuties")
+dog1=Dog()
+dog1.hairs()
+
+dog1.name="petty"
+dog1.age=12
