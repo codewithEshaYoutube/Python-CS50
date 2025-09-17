664fa13c5a3b27dbc1000a0a99af041e47e1aaa0
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-09-18 01:03:54 +0500

    Update Contructors.py

--- Contructors.py
+++ Contructors.py
@@ -44,5 +44,35 @@ print(list(enumerate(l1)))
 
 print (person1.age)
 
+class NumberPair:
+  def __init__(self,a,b):
+    self.a=a
+    self.b=b
+
+  def addNum(self):
+    return self.a+self.b
+  def multiplyNum(self):
+    return self.a*self.b
+# objects making
+pair1=NumberPair(3,6)
+pair2=NumberPair(13,5)
+
+print(" pair 1 -->  Multiply :",pair1.multiplyNum(), "Add:",pair1.addNum())
+print(" pair 2 -->  Multiply :",pair2.multiplyNum(), "Add:",pair2.addNum())
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        low, mid, high = 0, 0, len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:  # nums[mid] == 2
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
 
 
